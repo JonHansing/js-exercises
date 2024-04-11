21ddba03f86b04afa56750c976b9f32c179b705b
Jonathan Hansing <jon.hansing@gmail.com>
2024-04-11 07:27:55 -0500

    Finish 05-sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    if (startNum < 0 || endNum < 0 || typeof startNum != 'number' ||
+    typeof endNum != 'number') return 'ERROR';
+    
+    let result = 0;
 
+    const smallNum = Math.min(startNum, endNum);
+    const bigNum = Math.max(startNum, endNum);
+
+    for (let i = smallNum; i <= bigNum; i++) {
+        result += i;
+    };
+    return result;
 };
 
 // Do not edit below this line
