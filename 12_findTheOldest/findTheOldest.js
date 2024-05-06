8c9ee55d5cca6882bfcff5a1d033b37b9bee8f31
Jonathan Hansing <jon.hansing@gmail.com>
2024-05-06 07:45:47 -0500

    Complete 11-getTheTitles and 12-findTheOldest
    Done! findTheOldest is a little janky, but it passes the tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((oldest, currentItem) => {
+        let oldestAge = 0;
+        if (oldest.yearOfDeath == undefined) {
+            oldestAge = new Date().getFullYear() - oldest.yearOfBirth;
+        } else (oldestAge = oldest.yearOfDeath - oldest.yearOfBirth);
+        let currentAge = currentItem.yearOfDeath - currentItem.yearOfBirth;
+        if (currentAge > oldestAge) return currentItem;
+        else return oldest;
+    }, array[0]);
 };
 
 // Do not edit below this line
