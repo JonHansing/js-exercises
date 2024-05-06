bb1c2bb3b58263b0ce48d969429a92e149c99e6d
Jonathan Hansing <jon.hansing@gmail.com>
2024-05-06 07:11:41 -0500

    Complete 08-calculator, 09-palindromes, and 10-fib
    Done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let newStr = str
+        .toLowerCase()
+        .replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~\s+]/g, "");
+    let reverseStr = newStr
+        .split("")
+        .reverse()
+        .join("");
+    return newStr === reverseStr;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(inputNum) {
+    let num = Number(inputNum);
+    const arr = [1,1];
+    if (arr.length < num) {
+        for (let i = arr.length; i < num; i++) {
+            arr[i] = arr[i-1] + arr[i-2];
+        };
+    } else if (num == 0) {
+        return 0;
+    } else if (num < 1) {
+        return "OOPS";
+    };
+    return arr[num-1];
 };
 
 // Do not edit below this line
